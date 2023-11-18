becba22437643dd4121e4573a11a7b17e3a14959
Facundo Pascucci <percyers@outlook.com>
2023-11-18 13:45:32 -0300

    header

--- src/comp/Header/Header.jsx
+++ src/comp/Header/Header.jsx
@@ -1,6 +1,7 @@
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   return (
@@ -15,10 +16,9 @@ export default function Header() {
         <div>
           <h1>ToDo List</h1>
         </div>
-
-        <div>
-          <button className="options-button">...</button>
-        </div>
+        <button className="options-button">
+          <FontAwesomeIcon icon={faEllipsisVertical} />
+        </button>
 
     </div>
   )
