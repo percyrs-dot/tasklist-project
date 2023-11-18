b91dd770034bbb33a84eb15da6de3861ff2c6d5b
Facundo Pascucci <percyers@outlook.com>
2023-11-18 15:17:00 -0300

    finished basic functionality

--- src/comp/TaskList/TaskList.jsx
+++ src/comp/TaskList/TaskList.jsx
@@ -1,8 +1,40 @@
 import './TaskList.css'
-import {sampleNotes} from '../../assets/notes.json'
+import { useState, useEffect } from 'react';
 
 export default function TaskList() {
-  const notes = sampleNotes;
+
+    // Declaracion de las notas
+    const [notes, setNotes] = useState([]);
+
+    // Recibimos los datos de las notas
+    useEffect(() => {
+
+      const sampleNotes = [
+        { id: 1, content: 'Task 1', status: false },
+        { id: 2, content: 'Task 2', status: true },
+        { id: 3, content: 'Task 3', status: true },
+        { id: 4, content: 'Task 4', status: false },
+        { id: 5, content: 'Task 5', status: true }
+      ];
+
+      setNotes(sampleNotes);
+    }, []);
+  
+    // Setea en el array anterior uno sin la nota
+    // a la que le corresponde el id que le damos
+    const removeNote = (id) => {
+      setNotes(notes => notes.filter(note => note.id !== id));
+    };
+  
+    // Itera el array de notas, y si la nota coincide con el id que le damos
+    // hace una copia cuyo status es inverso. Si no, devuelve la misma nota.
+    const toggleStatus = (id) => {
+      setNotes(notes =>
+        notes.map(note =>
+          note.id === id ? { ...note, status: !note.status } : note
+        )
+      );
+    };
 
   return (
     <>
@@ -12,7 +44,20 @@ export default function TaskList() {
         <ol>
 
         {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+
+          <li key={note.id}>
+            
+            <input
+                type="checkbox"
+                checked={note.status}
+                onChange={() => toggleStatus(note.id)}
+              />
+
+            {note.content} - {note.status ? 'Completed' : 'Pending'}
+            
+            <button onClick={() => removeNote(note.id)}>X</button>
+          </li>
+
         ))}
         
         </ol>
